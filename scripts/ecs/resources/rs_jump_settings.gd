ff5ed7eb09146c6fdec14c896616cbbc8a45f062
dawson <mcthaydawson@gmail.com>
2025-10-29 01:35:59 -0500

    Tuning: Set new defaults for jump/floating (coyote/buffer=0.28, height_tol=0.10, settle_tol=0.30); update default .tres to match

--- scripts/ecs/resources/rs_jump_settings.gd
+++ scripts/ecs/resources/rs_jump_settings.gd
@@ -3,9 +3,9 @@ extends Resource
 class_name RS_JumpSettings
 
 @export var jump_force: float = 12.0
-@export var coyote_time: float = 0.15
+@export var coyote_time: float = 0.28
 @export var max_air_jumps: int = 0
-@export var jump_buffer_time: float = 0.15
+@export var jump_buffer_time: float = 0.28
 @export var apex_coyote_time: float = 0.1
 @export var apex_velocity_threshold: float = 0.1
 
