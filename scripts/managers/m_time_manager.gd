fc0d0204042bd9b67e32fe2e18de3d29ca6d50b6
dawson <mcthaydawson@gmail.com>
2026-02-17 18:10:30 -0600

    Add M_TimeManager with pause integration tests

---
+++ scripts/managers/m_time_manager.gd
@@ -0,0 +1,175 @@
+@icon("res://assets/editor_icons/icn_manager.svg")
+extends "res://scripts/interfaces/i_time_manager.gd"
+class_name M_TimeManager
+
+signal pause_state_changed(is_paused: bool)
+signal timescale_changed(new_scale: float)
+signal world_hour_changed(hour: int)
+
+const U_PAUSE_SYSTEM := preload("res://scripts/managers/helpers/time/u_pause_system.gd")
+
+var _store: I_StateStore = null
+var _cursor_manager: M_CursorManager = null
+var _ui_overlay_stack: CanvasLayer = null
+var _pause_system = U_PAUSE_SYSTEM.new()
+var _is_paused: bool = false
+var _current_scene_id: StringName = StringName("")
+var _current_scene_type: int = -1
+
+func _init() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS
+
+func _ready() -> void:
+	_store = U_ServiceLocator.try_get_service(StringName("state_store")) as I_StateStore
+	if not _store:
+		push_warning("M_TimeManager: M_StateStore not ready during _ready(). Deferring initialization.")
+		call_deferred("_deferred_init")
+		return
+	_initialize()
+
+func _deferred_init() -> void:
+	_store = U_ServiceLocator.try_get_service(StringName("state_store")) as I_StateStore
+	if not _store:
+		return
+	_initialize()
+
+func _initialize() -> void:
+	_cursor_manager = U_ServiceLocator.try_get_service(StringName("cursor_manager")) as M_CursorManager
+	_ui_overlay_stack = get_tree().root.find_child("UIOverlayStack", true, false) as CanvasLayer
+
+	_store.slice_updated.connect(_on_slice_updated)
+
+	var full_state: Dictionary = _store.get_state()
+	var scene_state: Dictionary = full_state.get("scene", {})
+	var scene_stack: Array = scene_state.get("scene_stack", [])
+	var ui_overlay_count: int = 0
+	if _ui_overlay_stack != null:
+		ui_overlay_count = _ui_overlay_stack.get_child_count()
+	var total_overlay_count: int = maxi(ui_overlay_count, scene_stack.size())
+	_pause_system.derive_pause_from_overlay_state(total_overlay_count)
+
+	_is_paused = _pause_system.compute_is_paused()
+	_current_scene_id = scene_state.get("current_scene_id", StringName(""))
+	_current_scene_type = _get_scene_type(_current_scene_id)
+
+	_apply_pause_and_cursor_state()
+
+func _exit_tree() -> void:
+	if _store and _store.slice_updated.is_connected(_on_slice_updated):
+		_store.slice_updated.disconnect(_on_slice_updated)
+
+func _process(__delta: float) -> void:
+	_check_and_resync_pause_state()
+
+func _check_and_resync_pause_state() -> void:
+	if not _store or not _ui_overlay_stack:
+		return
+
+	var current_ui_count: int = _ui_overlay_stack.get_child_count()
+	var scene_state: Dictionary = _store.get_slice(StringName("scene"))
+	var scene_stack: Array = scene_state.get("scene_stack", [])
+	var total_overlay_count: int = maxi(current_ui_count, scene_stack.size())
+	_pause_system.derive_pause_from_overlay_state(total_overlay_count)
+
+	var should_be_paused: bool = _pause_system.compute_is_paused()
+	if should_be_paused != _is_paused or get_tree().paused != _is_paused:
+		var pause_changed: bool = should_be_paused != _is_paused
+		_is_paused = should_be_paused
+		_apply_pause_and_cursor_state()
+		if pause_changed:
+			pause_state_changed.emit(_is_paused)
+
+func _on_slice_updated(slice_name: StringName, slice_state: Dictionary) -> void:
+	if slice_name != StringName("scene"):
+		return
+
+	var state_changed: bool = false
+	var pause_changed: bool = false
+
+	var scene_stack: Array = slice_state.get("scene_stack", [])
+	var ui_overlay_count: int = 0
+	if _ui_overlay_stack != null:
+		ui_overlay_count = _ui_overlay_stack.get_child_count()
+	var total_overlay_count: int = maxi(ui_overlay_count, scene_stack.size())
+	_pause_system.derive_pause_from_overlay_state(total_overlay_count)
+	var new_paused: bool = _pause_system.compute_is_paused()
+
+	if new_paused != _is_paused:
+		_is_paused = new_paused
+		state_changed = true
+		pause_changed = true
+
+	if get_tree().paused != _is_paused:
+		state_changed = true
+
+	var new_scene_id: StringName = slice_state.get("current_scene_id", StringName(""))
+	if new_scene_id != _current_scene_id:
+		_current_scene_id = new_scene_id
+		_current_scene_type = _get_scene_type(_current_scene_id)
+		state_changed = true
+
+	if state_changed:
+		_apply_pause_and_cursor_state()
+		if pause_changed:
+			pause_state_changed.emit(_is_paused)
+
+func _apply_pause_and_cursor_state() -> void:
+	get_tree().paused = _is_paused
+
+	if _cursor_manager:
+		if _is_paused:
+			_cursor_manager.set_cursor_state(false, true)
+		else:
+			match _current_scene_type:
+				U_SceneRegistry.SceneType.MENU, U_SceneRegistry.SceneType.UI, U_SceneRegistry.SceneType.END_GAME:
+					_cursor_manager.set_cursor_state(false, true)
+				U_SceneRegistry.SceneType.GAMEPLAY:
+					_cursor_manager.set_cursor_state(true, false)
+				_:
+					_cursor_manager.set_cursor_state(true, false)
+
+func _get_scene_type(scene_id: StringName) -> int:
+	if scene_id.is_empty():
+		return -1
+	var scene_data: Dictionary = U_SceneRegistry.get_scene(scene_id)
+	if scene_data.is_empty():
+		return -1
+	return scene_data.get("scene_type", -1)
+
+func is_paused() -> bool:
+	return _is_paused
+
+func request_pause(channel: StringName) -> void:
+	_pause_system.request_pause(channel)
+	_check_and_resync_pause_state()
+
+func release_pause(channel: StringName) -> void:
+	_pause_system.release_pause(channel)
+	_check_and_resync_pause_state()
+
+func is_channel_paused(channel: StringName) -> bool:
+	return _pause_system.is_channel_paused(channel)
+
+func get_active_pause_channels() -> Array[StringName]:
+	return _pause_system.get_active_channels()
+
+func set_timescale(_scale: float) -> void:
+	pass
+
+func get_timescale() -> float:
+	return 1.0
+
+func get_scaled_delta(raw_delta: float) -> float:
+	return raw_delta
+
+func get_world_time() -> Dictionary:
+	return {}
+
+func set_world_time(_hour: int, _minute: int) -> void:
+	pass
+
+func set_world_time_speed(_minutes_per_real_second: float) -> void:
+	pass
+
+func is_daytime() -> bool:
+	return true
--- tests/unit/managers/test_time_manager.gd
+++ tests/unit/managers/test_time_manager.gd
@@ -1,6 +1,30 @@
 extends GutTest
 
 const U_PAUSE_SYSTEM := preload("res://scripts/managers/helpers/time/u_pause_system.gd")
+const M_TIME_MANAGER := preload("res://scripts/managers/m_time_manager.gd")
+const MOCK_STATE_STORE := preload("res://tests/mocks/mock_state_store.gd")
+const U_SERVICE_LOCATOR := preload("res://scripts/core/u_service_locator.gd")
+
+var _time_manager: Node = null
+var _store: Node = null
+var _overlay_stack: CanvasLayer = null
+
+
+func before_each() -> void:
+	U_SERVICE_LOCATOR.clear()
+	_time_manager = null
+	_store = null
+	_overlay_stack = null
+	get_tree().paused = false
+
+func after_each() -> void:
+	get_tree().paused = false
+	if _overlay_stack and is_instance_valid(_overlay_stack):
+		_overlay_stack.free()
+	U_SERVICE_LOCATOR.clear()
+	_time_manager = null
+	_store = null
+	_overlay_stack = null
 
 
 func test_initial_state_not_paused() -> void:
@@ -99,3 +123,48 @@ func test_release_below_zero_clamps_and_ui_manual_noop() -> void:
 
 	pause_system.derive_pause_from_overlay_state(0)
 	assert_false(pause_system.is_channel_paused(U_PAUSE_SYSTEM.CHANNEL_UI), "Derive should clear UI channel")
+
+func test_backward_compat_pause_manager_lookup() -> void:
+	await _setup_time_manager_with_store()
+	U_SERVICE_LOCATOR.register(StringName("pause_manager"), _time_manager)
+
+	var pause_service := U_SERVICE_LOCATOR.get_service(StringName("pause_manager"))
+	assert_eq(pause_service, _time_manager, "pause_manager service should resolve to M_TimeManager")
+
+func test_is_paused_false_initially() -> void:
+	await _setup_time_manager_with_store()
+
+	assert_false(_time_manager.is_paused(), "Time manager should start unpaused")
+
+func test_request_release_pause() -> void:
+	await _setup_time_manager_with_store()
+
+	_time_manager.request_pause(U_PAUSE_SYSTEM.CHANNEL_CUTSCENE)
+	assert_true(_time_manager.is_paused(), "Requesting pause should set paused state")
+
+	_time_manager.release_pause(U_PAUSE_SYSTEM.CHANNEL_CUTSCENE)
+	assert_false(_time_manager.is_paused(), "Releasing pause should clear paused state")
+
+func test_pause_state_changed_signal() -> void:
+	await _setup_time_manager_with_store()
+	watch_signals(_time_manager)
+
+	_time_manager.request_pause(U_PAUSE_SYSTEM.CHANNEL_CUTSCENE)
+	_time_manager.release_pause(U_PAUSE_SYSTEM.CHANNEL_CUTSCENE)
+
+	assert_signal_emit_count(_time_manager, "pause_state_changed", 2,
+		"pause_state_changed should emit on pause and unpause transitions")
+
+func _setup_time_manager_with_store(scene_state: Dictionary = {}) -> void:
+	_store = MOCK_STATE_STORE.new()
+	_store.set_slice(StringName("scene"), scene_state)
+	add_child_autofree(_store)
+	U_SERVICE_LOCATOR.register(StringName("state_store"), _store)
+
+	_overlay_stack = CanvasLayer.new()
+	_overlay_stack.name = "UIOverlayStack"
+	add_child_autofree(_overlay_stack)
+
+	_time_manager = M_TIME_MANAGER.new()
+	add_child_autofree(_time_manager)
+	await get_tree().process_frame
