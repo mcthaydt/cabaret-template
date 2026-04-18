a2c54f7bda6b1dc3c5eb855e3a51f72e1eb0dbd2
dawson <mcthaydawson@gmail.com>
2026-04-17 19:49:32 -0500

    (RED+GREEN) P1.5 add RS_BTInverter decorator

---
+++ scripts/resources/bt/rs_bt_inverter.gd
@@ -0,0 +1,19 @@
+@icon("res://assets/editor_icons/icn_resource.svg")
+extends "res://scripts/resources/bt/rs_bt_decorator.gd"
+class_name RS_BTInverter
+
+func tick(context: Dictionary, state_bag: Dictionary) -> Status:
+	if child == null:
+		push_error("RS_BTInverter.tick: child is null")
+		return Status.FAILURE
+
+	var child_status: int = child.tick(context, state_bag)
+	if child_status == Status.SUCCESS:
+		return Status.FAILURE
+	if child_status == Status.FAILURE:
+		return Status.SUCCESS
+	if child_status == Status.RUNNING:
+		return Status.RUNNING
+
+	push_error("RS_BTInverter.tick: child returned invalid status %s" % str(child_status))
+	return Status.FAILURE
